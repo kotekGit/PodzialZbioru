856e3f2ab72ed24d8e0acaad824ac3880262c639
Mariusz Kielan <mariusz@kielan.org>
2014-05-08 22:02:26 +0200

    Dodanie testów jednostkowych dla permutacji

---
+++ PodzialZbioru/tests/pl/edu/wat/wcy/tal/exact/PermutationTest.java
@@ -0,0 +1,14 @@
+package pl.edu.wat.wcy.tal.exact;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PermutationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
