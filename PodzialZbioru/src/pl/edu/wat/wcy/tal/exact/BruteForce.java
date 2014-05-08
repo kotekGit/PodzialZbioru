7d7233b5cb276adc1f8f6617260d82454a575449
Mariusz Kielan <mariusz@kielan.org>
2014-05-08 03:08:25 +0200

    Trzon brute force zaimplementowany

--- PodzialZbioru/src/pl/edu/wat/wcy/tal/exact/BruteForce.java
+++ PodzialZbioru/src/pl/edu/wat/wcy/tal/exact/BruteForce.java
@@ -1,6 +1,8 @@
 package pl.edu.wat.wcy.tal.exact;
 
 import pl.edu.wat.wcy.tal.IOptPartition;
+import pl.edu.wat.wcy.tal.utils.Array;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class BruteForce implements IOptPartition {
 
@@ -9,8 +11,27 @@ public class BruteForce implements IOptPartition {
 	 */
 	@Override
 	public int partition(int[] array) {
-		// TODO Auto-generated method stub
-		return 0;
+		int totalSum = Array.sum(array);
+		
+		Permutation permutation = new Permutation(1, array.length);
+		
+		while(permutation.isNextPermutation()) {
+			Permutation tmp = permutation.getNextPartition();
+			
+			if(sumSelected(array, tmp) == totalSum / 2) return 0; 
+			
+			if(
+				Math.abs(totalSum - sumSelected(array, tmp)) 
+				< 
+				Math.abs(totalSum) - sumSelected(array, permutation)
+			)
+				permutation = tmp;
+		}
+		
+		return Math.abs(totalSum - 2 * sumSelected(array, permutation));
 	}
-
+	
+	public static int sumSelected(int[] array, Permutation permutation) {
+		throw new NotImplementedException();
+	}
 }
