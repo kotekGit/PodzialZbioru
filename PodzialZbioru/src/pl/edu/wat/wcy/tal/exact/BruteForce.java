2d823077dcf337aab9d5077670d66a7b52f7ffb7
Mariusz Kielan <mariusz@kielan.org>
2014-05-10 15:52:54 +0200

    Poprawny brute force

--- PodzialZbioru/src/pl/edu/wat/wcy/tal/exact/BruteForce.java
+++ PodzialZbioru/src/pl/edu/wat/wcy/tal/exact/BruteForce.java
@@ -9,28 +9,27 @@ import pl.edu.wat.wcy.tal.utils.Array;
  * @author Mariusz Kielan
  */
 public class BruteForce implements IOptPartition {
-	//TODO poprawić algorytm (nie wychodzi dla ostatniego przypadku z unit testów)
-	//TODO uwzględnić fakt, że problem jest symetryczny (połowa sprawdzeń)
+	
 	@Override
 	public int partition(int[] array) {
 		int totalSum = Array.sum(array);
 		
 		Permutation permutation = new Permutation(array);
-		int sum = permutation.sum();
+		int sum = totalSum;
 		
 		while(permutation.existNextPermutation()) {
 			permutation.next();
 			
-			if(permutation.sum() == totalSum / 2) return 0; 
+			if(permutation.sum() == totalSum / 2.) return 0; 
 			
 			if(
-				Math.abs(totalSum - sum) 
-				< 
-				Math.abs(totalSum) - permutation.sum()
+				Math.abs(totalSum/2. - sum)
+				>
+				Math.abs(totalSum/2. - permutation.sum()) 
 			)
 				sum = permutation.sum();
 		}
 		
-		return Math.abs(totalSum - 2 * sum);
+		return Math.abs(totalSum - 2*sum);
 	}
 }
